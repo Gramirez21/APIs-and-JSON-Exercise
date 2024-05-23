1d7fdbad1d038d552905c129688b22aa22baa32b
Gramirez21 <r.dannysixty@gmail.com>
2024-05-23 12:24:19 -0700

    api&json

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System;
+namespace APIsAndJSON
 {
     public class Program
     {
-        static void Main(string[] args)
+        
+
+        public static void Kanye()
         {
-            Console.WriteLine("Hello, World");
+            var client = new HttpClient();
+
+            var ye = "https://api.kanye.rest/";
+
+            var yeAnswer = client.GetStringAsync(ye).Result;
+
+            var yeGold = JObject.Parse(yeAnswer).GetValue("quote").ToString();
+
+            Console.WriteLine(yeGold);
+        }
+
+        public static void Ron() 
+        {
+            var client = new HttpClient();
+
+            var swonson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var swonsonAnswer = client.GetStringAsync(swonson).Result;
+            var swonsonQuote = JArray.Parse(swonsonAnswer).First().ToString();
+
+            Console.WriteLine(swonsonQuote);
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Weatherapp.Weather();
+         
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ron:"); Ron();
+                Console.WriteLine();
+                Console.WriteLine("Ye");  Kanye();
+                Console.WriteLine();
+            }
+
+            
+            
+
+          
+            
+
+            
+            
+
+            
+
+            
+
         }
     }
 }
---
+++ APIsAndJSON/Weatherapp.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace APIsAndJSON
+{
+    
+   
+    public class Weatherapp
+    {
+
+        public static void Weather()
+        {
+            var client = new HttpClient();
+
+            
+            string APIKey = "25b96530559420a386fde31ccde0dfeb";
+
+            Console.WriteLine("Please enter city name");
+            var cityName = Console.ReadLine();
+
+
+            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}&units=imperial";
+
+            
+
+
+            var response = client.GetStringAsync(apiCall).Result;
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            Console.WriteLine($"The Tempature in {cityName} is {temp}");
+
+        }
+    }
+}
